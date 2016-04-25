d73081c8199de80654e0f47a29095f79f8d1c9ab
Andy Hill <athill@iu.edu>
2016-04-24 20:53:58 -0400

    seems ot be working

--- __main__.py
+++ __main__.py
@@ -5,17 +5,16 @@ from subprocess import call, check_call
 from pprint import pprint
 
 ### 
-debug=True
+debug=False
 
+rootdir = os.path.abspath(os.sep)
 
+### set up app directory, if not set up
 homedir = os.path.expanduser('~') 
-
 appdir = os.path.join(homedir, '.bkup')
 if not os.path.exists(appdir):
     os.makedirs(appdir)
-
 configfile = os.path.join(appdir, 'config')
-
 template="""import os
 
 homedir = os.path.expanduser('~') 
@@ -28,46 +27,46 @@ profiles = {
 	}	
 }
 """
-
 if not os.path.exists(configfile+'.py'):
 	with open(configfile+'.py', "w") as text_file:
 		text_file.write(template)
 
 
+## load profiles
+sys.path.append(os.path.dirname(os.path.expanduser(configfile)))
+from config import profiles
+
+## arguments
+if len(sys.argv) != 2:
+	print("usage: bkup <profile>. Configured profiles are: "+', '.join(profiles.keys()))
+	exit(1)
+
+
+##  set up profiles
 defaults = {
 	'backupdir': homedir,
 	'destdir': os.path.join(homedir, 'tmp'),
 	'files': ['./.git-completion.bash','./.gitconfig','./test/a','./test/one.txt','./test/b/c.txt','./Code/provision/']
 }
-
-sys.path.append(os.path.dirname(os.path.expanduser(configfile)))
-from config import profiles
-
-if len(sys.argv) != 2:
-	print("usage: bkup <profile>. Configured profiles are: "+', '.join(profiles.keys()))
-	exit(1)
-
 profile = sys.argv[1]
-backupfile=profile+'.tgz'
-
+backupfile=os.path.join(rootdir, 'tmp', profile+'.tgz')
 if not profile in profiles.keys():
 	print("Error: Invalid profile, '"+profile+"'. Configured profiles are: "+', '.join(profiles.keys()))
 	exit(1)
-
 config = profiles[profile]
 for key in defaults.keys():
 	if not key in config:
 		config[key] = defaults[key]
 
-
-
+## if debugging, use ~/tmp as destdir and clear it
+destdir = config['destdir']
 if debug:
-	# rm -rf $destdir/*
-	shutil.rmtree(config['destdir'])
-	os.mkdir(config['destdir'])
+	destdir=os.path.join(homedir, 'tmp')
+	shutil.rmtree(destdir)
+	os.mkdir(destdir)
 
-# ## go to back up direcroty
-# cd $backupdir
+
+## go to back up direcroty
 os.chdir(config['backupdir'])
 
 
@@ -77,19 +76,15 @@ for name in config['files']:
 	tar.add(name)
 tar.close()
 
-destdir = config['destdir']
-if debug:
-	destdir=os.path.join(homedir, 'tmp')
-
+## create destdir it it doesn't exist
 if not os.path.exists(destdir):
-	os.path.mkdir(destdir)
+	os.mkdir(destdir)
 
-# ## move archive to destination
-shutil.move(backupfile, config['destdir'])
+## move archive to destination
+shutil.move(backupfile, destdir)
 
-# #### this part is for debugging, you could impement backup rotations, etc. here
 
-# cd destdir
+## if debugging, go to destdir (tmp) and unzip results
 if debug:
-	os.chdir(config['destdir'])
-	call(["tar", "xzvf", os.path.join(config['destdir'], backupfile)])
+	os.chdir(destdir)
+	call(["tar", "xzvf", os.path.join(destdir, backupfile)])
